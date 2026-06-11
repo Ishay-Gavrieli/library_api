e73afca8f87d935c51874d716b6a0b0a50b02c09
Ishay Gavrieli <hagit3172@gmail.com>
2026-06-11 15:01:57 +0300

    add logger to the project

---
+++ app/logs/basic_logger.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.INFO,format="%(asctime)s | %(levelname)s | %(message)s",
+                    handlers=[logging.FileHandler("app.log",encoding="utf-8"),
+                              logging.StreamHandler()])
+
+
+logger = logging.getLogger(__name__)
+
+
+if __name__=="__main__":
+    logger.info("start logger")
